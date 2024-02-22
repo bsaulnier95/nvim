559ed51382cb399d91fa1d500e9df005e3e74b48
Ben Saulnier <bsaulnier95@gmail.com>
2024-02-21 22:51:23 -0500

    LSP Update... still WIP

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -2,8 +2,16 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
  local on_attach = function(_, _)
-   vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-   vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
  end
 
  require("lspconfig").lua_ls.setup {}
