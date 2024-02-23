026926672dec648a842141494c4996ee14284da8
Ben Saulnier <bsaulnier95@gmail.com>
2024-02-22 19:35:14 -0500

    LSP complete.
    At least for now

--- after/plugin/lsp_config.lua
+++ after/plugin/lsp_config.lua
@@ -1,32 +1,39 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
- local on_attach = function(_, _)
-   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
--- vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references)
-   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
-   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
-   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end)
-   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
-   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
-   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
-   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
-   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
- end
+local on_attach = function(_, bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, bufopts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, bufopts)
+  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, bufopts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, bufopts)
+  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, bufopts)
+end
 
- require("lspconfig").lua_ls.setup {}
- require("lspconfig").eslint.setup {}
- require("lspconfig").tsserver.setup {}
- require("lspconfig").volar.setup {}
- require("lspconfig").solargraph.setup {
- 	capabilities = capabilities,
-   settings = {
-     solargraph = {
-       diagnostics = false
-     }
-   }
- }
- require("lspconfig").rubocop.setup {
-   cmd = { "rubocop", "-c", ".rubocop_todo.yml", "--lsp" }
- }
+-- Setup LSP servers
+require("lspconfig").lua_ls.setup {
+  on_attach = on_attach
+}
+require("lspconfig").eslint.setup { on_attach = on_attach }
+require("lspconfig").tsserver.setup { on_attach = on_attach }
+require("lspconfig").volar.setup { on_attach = on_attach }
+require("lspconfig").solargraph.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    solargraph = {
+      diagnostics = false
+    }
+  }
+}
+require("lspconfig").rubocop.setup {
+  on_attach = on_attach,
+  cmd = { "rubocop", "-c", ".rubocop_todo.yml", "--lsp" }
+}
+
