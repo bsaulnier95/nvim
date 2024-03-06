6ffed5a3051e49e89ba5dfa2d2d401df0345460a
Ben Saulnier <bsaulnier95@gmail.com>
2024-03-05 20:13:59 -0500

    Formatting

--- lua/prefs/packer.lua
+++ lua/prefs/packer.lua
@@ -5,56 +5,58 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use { 'wbthomason/packer.nvim' }
 
   --LSP
   use {
-	  'williamboman/mason.nvim',
-	  'williamboman/mason-lspconfig.nvim',
-	  'neovim/nvim-lspconfig'
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig'
   }
 
   -- Telescope
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   -- Color Schemes
-  use({
+  use{
 	  'folke/tokyonight.nvim',
 	  as = tokoyonight,
 	  config = function()
 		  vim.cmd('colorscheme tokyonight')
 	  end
-  })
- 
-  use({
+  }
+
+  use{
 	  'EdenEast/nightfox.nvim',
 	  as = nightfox,
 	  config = function()
 		  vim.cmd('colorscheme nightfox')
 	  end
-  })
-
+  }
 
   -- Tree sitter
   use {
 	'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
   }
+
   use { 'nvim-treesitter/playground' }
 
   -- Git
   use { 'tpope/vim-fugitive' }
-  use({
+  
+  use{
     "kdheepak/lazygit.nvim",
     -- optional for floating window border decoration
     requires = {
         "nvim-lua/plenary.nvim",
     },
-  })
+  }
+
   -- Undotree
-  use { 'mbbill/undotree' } 
+  use { 'mbbill/undotree' }
 end)
